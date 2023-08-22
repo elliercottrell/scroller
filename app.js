55c7164c69acdaff1667d443859c1c400c8754ea
elliercottrell <85800129+elliercottrell@users.noreply.github.com>
2023-08-22 20:50:17 +0100

    a different approach

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-const scrollContainer = document.querySelector("main");
+// const scrollContainer = document.querySelector("main");
 
-scrollContainer.addEventListener("wheel", (event) => {
-  event.preventDefault();
-  scrollContainer.scrollLeft += event.deltaY;
-});
+// scrollContainer.addEventListener("wheel", (event) => {
+//   event.preventDefault();
+//   scrollContainer.scrollLeft += event.deltaY;
+// });
+
+let items = document.querySelectorAll(".item");
+let slider = document.querySelector(".slider");
+let sliderWidth;
+let itemWidth;
+let currentPos = 0;
+
+function init() {
+  sliderWidth = slider.getBoundingClientRect().width;
+  itemWidth = sliderWidth / items.length;
+  document.body.style.height = `${
+    sliderWidth - (window.innerWidth - window.innerHeight)
+  }px`;
+}
+
+function setSliderWidth() {
+  let totalWidth = 0;
+  items.forEach((item) => {
+    totalWidth += item.offsetWidth;
+  });
+
+  slider.style.width = `${totalWidth}px`;
+}
+
+function animate() {
+  init();
+  setSliderWidth();
+  currentPos = window.scrollY;
+  slider.style.transform = `translateX(${-currentPos}px)`;
+  requestAnimationFrame(animate);
+}
+
+animate();
